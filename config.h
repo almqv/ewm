9ab995871b0eb44a03973645ceefc18bc5a13022
E. Almqvist <elalmqvist@gmail.com>
2020-11-19 21:52:51 +0100

    Bar height & reset colors

--- config.h
+++ config.h
@@ -22,17 +22,17 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-/* static const char *colors[][3]      = { */
-/* 	/\*               fg         bg         border   *\/ */
-/* 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 }, */
-/* 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }, */
-/* }; */
-
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { text_color, bg_color_bar, bg_color },
-	[SchemeSel]  = { text_color, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
+
+/* static const char *colors[][3]      = { */
+/* 	/\*               fg         bg         border   *\/ */
+/* 	[SchemeNorm] = { text_color, bg_color_bar, bg_color }, */
+/* 	[SchemeSel]  = { text_color, col_cyan,  col_cyan  }, */
+/* }; */
 
 
 /* tagging */
