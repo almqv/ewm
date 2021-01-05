6f976f043104edff8c52ea51bbe4abf341512abc
E. Almqvist <elalmqvist@gmail.com>
2021-01-05 15:09:37 +0100

    Made floating mode default

--- src/config.h
+++ src/config.h
@@ -59,8 +59,8 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "><>",      NULL },    /* first entry is default */
+	{ "[]=",      tile },    
 	{ "[M]",      monocle },
 };
 
