153514de087b48f415264f10eeba2b60dfcbe9df
E. Almqvist <elalmqvist@gmail.com>
2021-01-07 02:14:23 +0100

    Steam floating

--- src/config.h
+++ src/config.h
@@ -51,6 +51,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "discord",  NULL,       NULL,       0,            1,           -1 },
+	{ "Steam",    NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
