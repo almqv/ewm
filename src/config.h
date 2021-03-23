66b18ec521b4a882746f619f5c7f297d68498963
E. Almqvist <elalmqvist@gmail.com>
2021-03-23 10:05:04 +0100

    Added sizeing for gaps

--- src/config.h
+++ src/config.h
@@ -116,6 +116,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Print,  spawn,          {.v = spectaclecmd} },
 	{ MODKEY,                       XK_u,      spawn,          {.v = cmuspausecmd} },
 	{ MODKEY,                       XK_g,      togglegaps,     {0} },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+    { MODKEY,                       XK_plus,   setgaps,		   {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
