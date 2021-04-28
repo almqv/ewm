f25b98b0cd46046d853632a0a3c0aa9b893ba663
E. Almqvist <elalmqvist@gmail.com>
2021-04-28 13:44:35 +0200

    Revert to 10 tags

--- src/config.h
+++ src/config.h
@@ -36,7 +36,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "I", "II", "III", "IV", "V" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
