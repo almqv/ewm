775393f82492f3eea21ceea5958fe686439f6fb2
E. Almqvist <elalmqvist@gmail.com>
2021-06-05 13:30:23 +0200

    dmenu bug fix

--- src/config.h
+++ src/config.h
@@ -74,7 +74,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", bg_color, "-nf", text_color, "-sb", col_cyan, "-sf", col_gray4, "-p", ">", "-i", "-l", "4", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *betterlockscreencmd[] = { "betterlockscreen", "--lock", "blur", NULL };
 static const char *spectaclecmd[] = { "spectacle", "-r", "-c", "-b", NULL };
