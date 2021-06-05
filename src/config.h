997db4cedafe56d617459a15cb815f9b49d9a9b9
E. Almqvist <elalmqvist@gmail.com>
2021-06-05 13:34:30 +0200

    Minor tweak

--- src/config.h
+++ src/config.h
@@ -92,7 +92,7 @@ static Key keys[] = {
 	{ MODKEY,						XK_h,	   setmfact,	   {.f = -0.05} },
 	{ MODKEY,						XK_l,	   setmfact,	   {.f = +0.05} },
 	{ MODKEY|ShiftMask,				XK_Return, zoom,		   {0} },
-	{ MODKEY,						XK_Tab,    view,		   {0} },
+	{ MODKEY,						XK_Tab,	   view,		   {0} },
 	{ MODKEY|ShiftMask,				XK_q,	   killclient,	   {0} },
 	{ MODKEY,						XK_t,	   setlayout,	   {.v = &layouts[0]} },
 	{ MODKEY,						XK_f,	   setlayout,	   {.v = &layouts[1]} },
