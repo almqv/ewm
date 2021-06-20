c7d728b943459ca15b981ee8ba3d3cbaefdb632c
E. Almqvist <elalmqvist@gmail.com>
2021-06-20 21:59:02 +0200

    Removed gaps (temp)

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx	= 4;		/* border pixel of windows */
 static const unsigned int snap		= 16;		/* snap pixel (32)*/
-static const unsigned int gappx		= 40;
+static const unsigned int gappx		= 0;		/* px: 40 */
 static const int showbar			= 1;		/* 0 means no bar */
 static const int topbar				= 0;		/* 0 means bottom bar */
 static const int usealtbar			= 1;		/* 1 means use non-dwm status bar */
