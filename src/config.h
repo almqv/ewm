8cef5722da93d3e2a54c442370298650758ee55d
E. Almqvist <elalmqvist@gmail.com>
2021-08-15 14:04:51 +0200

    Font change & topbar

--- src/config.h
+++ src/config.h
@@ -5,24 +5,24 @@ static const unsigned int borderpx	= 4;		/* border pixel of windows */
 static const unsigned int snap		= 16;		/* snap pixel (32)*/
 static const unsigned int gappx		= 0;
 static const unsigned int gapmodes[] = { gappx, 40, 80 };
-static const int showbar			= 1;		/* 0 means no bar */
-static const int topbar				= 0;		/* 0 means bottom bar */
-static const int usealtbar			= 1;		/* 1 means use non-dwm status bar */
+static const int showbar		= 1;		/* 0 means no bar */
+static const int topbar			= 1;		/* 0 means bottom bar */
+static const int usealtbar		= 1;		/* 1 means use non-dwm status bar */
 
 static const char *altbarclass		= "Polybar"; /* Alternate bar class name */
 static const char *alttrayname		= "tray";	 /* Polybar tray instance name */
 static const char *altbarcmd		= "$HOME/.config/polybar/launch"; /* Alternate bar launch command */
-static const char *fonts[]			= { "Fira Code:size=10" };
-static const char dmenufont[]		= "Fira Code:size=12";
+static const char *fonts[]		= { "monospace:size=10" };
+static const char dmenufont[]		= "monospace:size=12";
 
-static const char bg_color[]			= "#111015";
-static const char bg_color_bar[]		= "#111015";
+static const char bg_color[]		= "#111015";
+static const char bg_color_bar[]	= "#111015";
 static const char inactive_bg_color[]	= "#272727";
-static const char text_color[]			= "#d8dee9";
+static const char text_color[]		= "#d8dee9";
 static const char inactive_text_color[]	= "#676E7D";
-static const char urgent_bg_color[]		= "#E53935";
-static const char indicator_color[]		= "#474747";
-static const char selected_color[]		= "#81a2be";
+static const char urgent_bg_color[]	= "#E53935";
+static const char indicator_color[]	= "#474747";
+static const char selected_color[]	= "#81a2be";
 
 static const char col_gray1[]		= "#222222";
 static const char col_gray2[]		= "#444444";
@@ -30,7 +30,7 @@ static const char col_gray3[]		= "#bbbbbb";
 static const char col_gray4[]		= "#eeeeee";
 static const char col_cyan[]		= "#5f819d";
 static const char *colors[][3]		= {
-	/*				 fg			bg		   border	*/
+	/*		fg		bg	   border	*/
 	[SchemeNorm] = { col_gray3, bg_color_bar, col_gray1 },
 	[SchemeSel]  = { col_gray4, col_cyan,  selected_color },
 };
@@ -45,10 +45,10 @@ static const Rule rules[] = {
 	 */
 	/* class	  instance	  title		  tags mask		isfloating	 monitor */
 	{ "Gimp",	  NULL,		  NULL,		  0,			1,			 -1 },
-	{ "Firefox",  NULL,		  NULL,		  1 << 8,		0,			 -1 },
-	{ "discord",  NULL,		  NULL,		  0,			1,			 -1 },
+	{ "Firefox",	  NULL,		  NULL,		  1 << 8,		0,			 -1 },
+	{ "discord",	  NULL,		  NULL,		  0,			1,			 -1 },
 	{ "Steam",	  NULL,		  NULL,		  0,			1,			 -1 },
-	{ "Qemu-system-x86_64",	  NULL,		  NULL,		  0,			1,			 -1 },
+	{ "Qemu-system-x86_64",	  NULL,	  NULL,		  0,			1,			 -1 },
 };
 
 /* layout(s) */
@@ -96,19 +96,19 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,				XK_Return, zoom,		   {0} },
 	{ MODKEY,						XK_Tab,	   view,		   {0} },
 	{ MODKEY|ShiftMask,				XK_q,	   killclient,	   {0} },
-	{ MODKEY,                       XK_Down,   moveresize,     {.v = "0x 25y 0w 0h" } },
-	{ MODKEY,                       XK_Up,     moveresize,     {.v = "0x -25y 0w 0h" } },
-	{ MODKEY,                       XK_Right,  moveresize,     {.v = "25x 0y 0w 0h" } },
-	{ MODKEY,                       XK_Left,   moveresize,     {.v = "-25x 0y 0w 0h" } },
-	{ MODKEY|ShiftMask,             XK_Down,   moveresize,     {.v = "0x 0y 0w 25h" } },
-	{ MODKEY|ShiftMask,             XK_Up,     moveresize,     {.v = "0x 0y 0w -25h" } },
-	{ MODKEY|ShiftMask,             XK_Right,  moveresize,     {.v = "0x 0y 25w 0h" } },
-	{ MODKEY|ShiftMask,             XK_Left,   moveresize,     {.v = "0x 0y -25w 0h" } },
-	{ MODKEY|ControlMask,           XK_Up,     moveresizeedge, {.v = "t"} },
-	{ MODKEY|ControlMask,           XK_Down,   moveresizeedge, {.v = "b"} },
-	{ MODKEY|ControlMask,           XK_Left,   moveresizeedge, {.v = "l"} },
-	{ MODKEY|ControlMask,           XK_Right,  moveresizeedge, {.v = "r"} },
-	{ MODKEY|ControlMask|ShiftMask, XK_Up,     moveresizeedge, {.v = "T"} },
+	{ MODKEY,			XK_Down,   moveresize,	   {.v = "0x 25y 0w 0h" } },
+	{ MODKEY,			XK_Up,	   moveresize,	   {.v = "0x -25y 0w 0h" } },
+	{ MODKEY,			XK_Right,  moveresize,	   {.v = "25x 0y 0w 0h" } },
+	{ MODKEY,			XK_Left,   moveresize,	   {.v = "-25x 0y 0w 0h" } },
+	{ MODKEY|ShiftMask,		XK_Down,   moveresize,	   {.v = "0x 0y 0w 25h" } },
+	{ MODKEY|ShiftMask,		XK_Up,	   moveresize,	   {.v = "0x 0y 0w -25h" } },
+	{ MODKEY|ShiftMask,		XK_Right,  moveresize,	   {.v = "0x 0y 25w 0h" } },
+	{ MODKEY|ShiftMask,		XK_Left,   moveresize,	   {.v = "0x 0y -25w 0h" } },
+	{ MODKEY|ControlMask,		XK_Up,	   moveresizeedge, {.v = "t"} },
+	{ MODKEY|ControlMask,		XK_Down,   moveresizeedge, {.v = "b"} },
+	{ MODKEY|ControlMask,		XK_Left,   moveresizeedge, {.v = "l"} },
+	{ MODKEY|ControlMask,		XK_Right,  moveresizeedge, {.v = "r"} },
+	{ MODKEY|ControlMask|ShiftMask, XK_Up,	   moveresizeedge, {.v = "T"} },
 	{ MODKEY|ControlMask|ShiftMask, XK_Down,   moveresizeedge, {.v = "B"} },
 	{ MODKEY|ControlMask|ShiftMask, XK_Left,   moveresizeedge, {.v = "L"} },
 	{ MODKEY|ControlMask|ShiftMask, XK_Right,  moveresizeedge, {.v = "R"} },
