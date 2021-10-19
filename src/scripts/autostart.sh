4239f88c306c7ee36cca612997b2d3f19285a3ef
E. Almqvist <elalmqvist@gmail.com>
2021-10-19 21:10:48 +0200

    Stuff

--- src/scripts/autostart.sh
+++ src/scripts/autostart.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 while true; do
-	xsetroot -name "$(date +"%H:%M:%S  (%A) %Y-%m-%d")"
+	CMUS="$(cmus.sh)"
+	NET="up: 100b/s | down: 999kb/s"
+	VOL=$(pavolume.sh)
+	TIME="{$(date +"%H:%M:%S, %A, %Y-%m-%d")} "
+
+	xsetroot -name " $CMUS    $NET    Vol: $VOL     $TIME"
 	sleep 1s
-done
+done &
