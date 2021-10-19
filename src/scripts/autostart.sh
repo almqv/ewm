a8dc0eefc2712435f1d32dff61898ffcb3c22ebd
E. Almqvist <elalmqvist@gmail.com>
2021-10-19 22:11:26 +0200

    Stuff

--- src/scripts/autostart.sh
+++ src/scripts/autostart.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-while true; do
-	CMUS="$(cmus.sh)"
-	NET="up: 100b/s | down: 999kb/s"
-	VOL=$(pavolume.sh)
-	TIME="{$(date +"%H:%M:%S, %A, %Y-%m-%d")} "
-
-	xsetroot -name " $CMUS    $NET    Vol: $VOL     $TIME"
-	sleep 1s
-done &
+# dwmblocks > /tmp/dwmblocks.log &
