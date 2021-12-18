9a6848c40fd771f9f8680bd1b059675a18d9b5fa
E. Almqvist <elalmqvist@gmail.com>
2021-12-18 14:02:03 +0100

    Minor tweak

--- src/config.h
+++ src/config.h
@@ -5,10 +5,10 @@
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* appearance */
-static const unsigned int borderpx	= 2;		/* border pixel of windows */
+static const unsigned int borderpx	= 0;		/* border pixel of windows */
 static const unsigned int snap		= 16;		/* snap pixel (32)*/
-static const unsigned int gappx		= 64;
-static const unsigned int gapmodes[] 	= { gappx, 0 };
+static const unsigned int gappx		= 0;
+static const unsigned int gapmodes[] 	= { 0, 64 };
 static const int showbar		= 1;		/* 0 means no bar */
 static const int topbar			= 1;		/* 0 means bottom bar */
 static const int usealtbar		= 0;		/* 1 means use non-dwm status bar */
