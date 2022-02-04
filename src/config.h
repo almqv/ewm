aab07cf6f7b5aa2c108f415028afd8b7de239e39
E. Almqvist <elalmqvist@gmail.com>
2022-02-04 16:54:47 +0100

    Demenu title tweak

--- src/config.h
+++ src/config.h
@@ -77,7 +77,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", "$>", "-z", "512", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", "Run $", "-z", "512", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *betterlockscreencmd[] = { "betterlockscreen", "--lock", "blur", NULL };
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
