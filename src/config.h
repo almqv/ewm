88f622ffcb6e994a1f8c6d7187590bf34747c099
E. Almqvist <elalmqvist@gmail.com>
2022-08-15 12:44:25 +0200

    Tweaks

--- src/config.h
+++ src/config.h
@@ -4,9 +4,9 @@
 #define STATUSBAR "dwmblocks"
 /* appearance */
 static const unsigned int borderpx	= 2;			/* border pixel of windows */
-static const unsigned int snap		= 16;			/* snap pixel (32)*/
+static const unsigned int snap		= 0;			/* snap pixel (32)*/
 static const unsigned int gappx		= 0;
-static const unsigned int gapmodes[] = { 0, 64 };
+static const unsigned int gapmodes[] = { 0, 20 };
 static const int showbar			= 1;			/* 0 means no bar */
 static const int topbar				= 1;			/* 0 means bottom bar */
 static const int barverticalpadding = 6; 			/* Vertical bar padding  */
