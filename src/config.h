8ceff1ac718e3d9933077ddc466d98f16f69475d
Elias Almqvist <elalmqvist@gmail.com>
2024-06-09 02:16:38 +0200

    more fixes

--- src/config.h
+++ src/config.h
@@ -100,7 +100,6 @@ static const char *browsercmd[]          = {"firefox", NULL};
 static const char *betterlockscreencmd[] = {"betterlockscreen", "--lock",
                                             "blur", NULL};
 static const char *screenshotcmd[]       = {"flameshot", "gui", NULL};
-static const char *playctl_playpausecmd[]        = {"playctl", "play-pause", NULL};
 
 static Key keys[] = {
     /* modifier						key
@@ -166,7 +165,7 @@ static Key keys[] = {
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_l, spawn, {.v = betterlockscreencmd}},
     {MODKEY, XK_Print, spawn, {.v = screenshotcmd}},
-    {MODKEY, XK_u, spawn, {.v = playctl_playpausecmd}},
+    {MODKEY, XK_u, spawn, SHCMD("playctl play-pause")},
     {MODKEY, XK_g, switchgaps, {.i = +1}},
     {MODKEY, XK_v, switchgaps, {.i = -1}},
     {MODKEY, XK_minus, setgaps, {.i = -1}},
